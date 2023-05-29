4cfe868d9c8907445b18c9e0869e62752b3f355f
SasVel <sasvelchev@gmail.com>
2023-05-29 09:33:48 +0300

    UI implementation

---
+++ UI/CreditsUI/CreditsUI.gd
@@ -0,0 +1,4 @@
+extends Control
+
+func _on_back_btn_pressed():
+	get_tree().change_scene_to_file("res://UI/MainMenuUI/MainMenuUI.tscn")
--- UI/MainMenuUI/MainMenuUI.gd
+++ UI/MainMenuUI/MainMenuUI.gd
@@ -5,7 +5,7 @@ func _on_play_btn_pressed():
 	get_tree().change_scene_to_file("res://World/Main.tscn")
 
 func _on_settings_btn_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://UI/SettingsUI/SettingsUI.tscn")
 
 func _on_credits_btn_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://UI/CreditsUI/CreditsUI.tscn")

636678362b450dca75863e937d765080d91b2588
SasVel <sasvelchev@gmail.com>
2023-06-08 07:52:53 +0300

    Resource loader abstraction

---
+++ GlobalLoader.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+@onready var BaseBullet = preload("res://Bullets/MagicBullet.tscn")
+@onready var BigBullet = preload("res://Bullets/BigMagicBullet.tscn")
+@onready var ExplosionBullet = preload("res://Bullets/ExplodingMagicBullet.tscn")
--- Player/SpellsController.gd
+++ Player/SpellsController.gd
@@ -1,8 +1,6 @@
 extends Node2D
 
-@onready var BaseBullet = preload("res://Bullets/MagicBullet.tscn")
-@onready var BigBullet = preload("res://Bullets/BigMagicBullet.tscn")
-@onready var ExplosionBullet = preload("res://Bullets/ExplodingMagicBullet.tscn")
+
 
 @onready var shootTimer = $ShootTimer
 @onready var staff
